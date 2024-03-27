5ee8d340b474690c9f63d7f2e6dd06ebd403ecb6
manolli kassabian <manollikassabian3@gmail.com>
2024-03-27 22:38:46 +0200

    small changes in style

--- src/components/Image-Details/ImageDetails.tsx
+++ src/components/Image-Details/ImageDetails.tsx
@@ -1,65 +1,107 @@
-import React, { useEffect } from 'react'
-import ImageComponent from '../Image/image'
-import { RootStackParamList } from '../../types/navigation';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RouteProp } from '@react-navigation/native';
-import { Image, Text, View } from 'react-native';
-import { Dimensions, StyleSheet } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Animated, Easing, Dimensions, StyleSheet, View, TouchableOpacity, Image, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons'; // Import the desired icon from react-native-vector-icons
 
 const { width, height } = Dimensions.get('window');
 
-interface ImageDetailsProps {
-    navigation?: StackNavigationProp<RootStackParamList, 'ImageDetails'>;
-    route?: RouteProp<RootStackParamList, 'ImageDetails'>;
+const ImageDetails = ({ route }:any) => {
+    const navigation = useNavigation();
+    const image = route?.params;
+    const slideAnim = useRef(new Animated.Value(height)).current;
+    const fadeAnim = useRef(new Animated.Value(0)).current;
 
-  }
-  const ImageDetails:React.FC<ImageDetailsProps> =({ navigation, route })=> {
-    const image = route?.params;  
-  console.log(Number(image?.['latitude']));
-  
-  return (
-    <View style={styles.container}>
-    <Image style={styles.img} source={{ uri: image?.['name'] }} />
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: Number(image?.['latitude']),
-          longitude: Number(image?.['longitude']),
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          coordinate={{
-            latitude: Number(image?.['latitude']),
-            longitude: Number(image?.['longitude']),
-          }}
-          title="Your Location"
-          description="This is your location"
-        />
-      </MapView>
-    </View>
-  </View>
-  
-  )
-}
+    useEffect(() => {
+        Animated.parallel([
+            Animated.timing(
+                slideAnim,
+                {
+                    toValue: 0,
+                    duration: 1000,
+                    easing: Easing.out(Easing.cubic),
+                    useNativeDriver: true,
+                }
+            ),
+            Animated.timing(
+                fadeAnim,
+                {
+                    toValue: 1,
+                    duration: 1000,
+                    easing: Easing.linear,
+                    useNativeDriver: true,
+                }
+            )
+        ]).start();
+    }, [fadeAnim, slideAnim]);
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+              <Text>
+                <Icon name="arrow-back" size={24} color="black" /> {/* Use the icon component with desired name, size, and color */}
+                </Text>
+            </TouchableOpacity>
+            <Animated.View style={[styles.contentContainer, { opacity: fadeAnim }]}>
+                <Animated.View style={[styles.imageContainer, { transform: [{ translateY: slideAnim }] }]}>
+                    <Image style={styles.image} source={{ uri: image?.name }} />
+                </Animated.View>
+                <View style={styles.mapContainer}>
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            latitude: Number(image?.latitude),
+                            longitude: Number(image?.longitude),
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                    >
+                        <Marker
+                            coordinate={{
+                                latitude: Number(image?.latitude),
+                                longitude: Number(image?.longitude),
+                            }}
+                            title="Your Location"
+                            description="This is your location"
+                        />
+                    </MapView>
+                </View>
+            </Animated.View>
+        </View>
+    );
+};
+
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-    backgroundColor:'white',
+        flex: 1,
+        backgroundColor: 'white',
     },
-    img:{
-        width:width,
-        height:height/3,  
+    contentContainer: {
+        flex: 1,
     },
-    text: {
-        fontSize: 18,
-        marginTop: 10,
-      },
-      map: {
+    backButton: {
+        position: 'absolute',
+        top: 16,
+        left: 16,
+        zIndex: 1,
+    },
+    imageContainer: {
+        height: height / 3,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        marginBottom: 10,
+        overflow: 'hidden',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    mapContainer: {
+        flex: 1,
+    },
+    map: {
         ...StyleSheet.absoluteFillObject,
-      },
-  });
+    },
+});
+
 export default ImageDetails;
